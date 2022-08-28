9fb16e818fa70829a9e46a7d725a6665283d464c
maximenorval <maximenorval@gmail.com>
2022-08-28 16:56:07 +0200

    first design of the score buttons

--- src/app/components/match/current-match/score-buttons/score-buttons.component.ts
+++ src/app/components/match/current-match/score-buttons/score-buttons.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-score-buttons',
   templateUrl: './score-buttons.component.html',
-  styleUrls: ['./score-buttons.component.scss']
+  styleUrls: ['./score-buttons.component.scss'],
 })
 export class ScoreButtonsComponent implements OnInit {
+  players: any[] = [
+    { name: 'Federer', isServing: true },
+    { name: 'Nadal', isServing: false }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
-const materialModules: any[] = [MatTableModule, MatIconModule];
+const materialModules: any[] = [MatTableModule, MatIconModule, MatButtonModule];
 
 @NgModule({
   imports: [...materialModules],
