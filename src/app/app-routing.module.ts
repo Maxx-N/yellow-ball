7ae3b20c3841819441a852fbf94eecdc1713138e
maximenorval <maximenorval@gmail.com>
2022-08-28 14:52:42 +0200

    adding routes children of 'match'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { MatchComponent } from './components/match/match.component';
 import { PlayerComponent } from './components/player/player.component';
+import { CurrentMatchComponent } from './components/match/current-match/current-match.component';
+import { MatchListComponent } from './components/match/match-list/match-list.component';
 
 const routes: Routes = [
-  { path: 'match', component: MatchComponent },
+  {
+    path: 'match',
+    component: MatchComponent,
+    children: [
+      { path: 'current', component: CurrentMatchComponent },
+      { path: 'all', component: MatchListComponent },
+      { path: '**', redirectTo: 'current' },
+    ],
+  },
   { path: 'player', component: PlayerComponent },
   { path: '**', redirectTo: 'match' },
 ];
